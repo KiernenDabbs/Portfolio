f3b2925634e39712f191adb06086c40c8c4fa13e
KiernenDabbs <Kiernen_Dabbs@insideranken.org>
2025-02-19 13:59:48 -0600

    vercel analytics implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { cn } from "@/lib/utils"
+import { Analytics } from "@vercel/analytics/react"
 import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
